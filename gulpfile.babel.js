ca859bd5540fbdfc0b46760a651c22ba37e7388f
Trev <trevor.pawlewicz@gmail.com>
2016-09-28 18:41:40 -0400

    added public, private and helper tasks to gulpfile.babel.js

---
+++ gulpfile.babel.js
@@ -0,0 +1,53 @@
+import gulp from "gulp";
+import webpack from "webpack";
+import chalk from "chalk";
+import rimraf from "rimraf";
+import {create as createServerConfig} from "./webpack.server";
+
+const $ = require("gulp-load-plugins")();
+
+console.log(`===> in the gulp.babel.js file!`);
+
+
+// PUBLIC TASKS ---------------------------------------------------------------
+gulp.task("clean:server", callback => rimraf("./build", callback));
+gulp.task("clean:client", callback => rimraf("./public/build", callback));
+gulp.task("clean", gulp.parallel("clean:server", "clean:client"));
+
+gulp.task("dev:server", gulp.series("clean:server", devServerBuild));
+gulp.task("prod:server", gulp.series("clean:server", prodServerBuild));
+
+
+
+// PRIVATE SERVER TASKS -------------------------------------------------------
+const devServerWebpack = webpack(createServerConfig(true));//from webpack.server.js export
+const prodServerWebpack = webpack(createServerConfig(false));
+
+function devServerBuild (callback) {
+    devServerWebpack.run((error, stats) => {
+        outputWebpack("Dev:Server", error, stats);
+        callback(); // gulp needs to be told we are done with task
+    });
+};
+
+function prodServerBuild (callback) {
+    prodServerWebpack.run((error, stats) => {
+        outputWebpack("Prod:Server", error, stats);
+        callback(); // gulp needs to be told we are done with task
+    });
+};
+
+
+
+
+// HELPERS --------------------------------------------------------------------
+function outputWebpack (label, error, stats) {
+    if (error) { throw new Error(error); }
+
+    if (stats.hasErrors()) {
+        $.util.log(stats.toString({colors: true}));
+    } else {
+        const time = stats.endTime - stats.startTime;
+        $.util.log(chalk.bgGreen(`Built ${label} in ${time} ms!`));
+    }
+};
